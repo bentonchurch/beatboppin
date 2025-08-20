3950dacc297844e427fe21f8760692bb019d9e44
Eli <136011323+eliaselliotson@users.noreply.github.com>
2025-08-20 00:18:41 +0000

    Temporarily reworked the samples.js file

--- src/player/samples.js
+++ src/player/samples.js
@@ -1,17 +1,17 @@
 export const baseUrl = "./samples/";
 
+export const manifest = JSON.parse(await (await fetch(`${baseUrl}/manifest.json`)).text());
+
 export const aliases = {
   metronome: "C1",
   kicker: "D1",
   snare: "E1",
-  hihat_closed: "F1",
-  cymbal: "G1"
+  hihat_closed: "F1"
 };
 
 export const samples = {
-  C1: "metronome.mp3",
-  D1: "dance/kick.ogg",
-  E1: "dance/snare.ogg",
-  F1: "dance/hihat-closed.ogg",
-  G1: "dance/cymbal.mp3",
+  C1: manifest.metronome.samples[0],
+  D1: manifest.kick.samples[0],
+  E1: manifest.snare.samples[0],
+  F1: manifest.hi_hat_closed.samples[0],
 };
